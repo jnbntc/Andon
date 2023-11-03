47333341ee4fadd06f9526c79343587c7a905855
Adrian Reggiani <97036576+lopoadrian@users.noreply.github.com>
2023-11-03 09:54:50 -0300

    Se modifico los funcionamientos de los Led
    Se modifico el funcionamiento de los LED Verde para que parpadee. LED Amarillo para que se mantenga apagado hasta que reciba un código y parpadee cuando este correcto. Led Rojo para que parpadee si no encuentra ninguna pistola conectada

--- main.cpp
+++ main.cpp
@@ -59,26 +59,28 @@ int main(int argc, char *argv[]) {
 	//std::cout << "inputDevPath_Unitech: " << inputDevPath_Unitech << std::endl;
 	//std::cout << "idScanner: " << idScanner << std::endl;
 	//std::cout << "sendIDScanner: " << sendIDScanner << std::endl;
-
+  
 	udpInit(argv[1], stoi(argv[2]));
 
 	thread barcodeReaderThread(barcodeReaderTask);
+  thread udpRecvThread(udpRecvTask);
 	thread udpSendThread(udpSendTask);
-	thread udpRecvThread(udpRecvTask);
-
+	
+ 
 	barcodeReaderThread.join();
+  udpRecvThread.join();
 	udpSendThread.join();
-	udpRecvThread.join();
+	
 
 	return EXIT_SUCCESS;
 }
 
 // función para leer la ruta del scanner desde el archivo de configuración
 void readConfigFile() {
-    std::ifstream configFile("/home/pi/andon/config.ini");
+    std::ifstream configFile("/home/pi/andon/config.ini"); // std::ifstream abre y lee el archivo config
     if (configFile.is_open()) {
         std::string line;
-        while (std::getline(configFile, line)) {
+        while (std::getline(configFile, line)) {//std::getline lee cada linea del archivo config.
             size_t equals_pos = line.find('=');
             if (equals_pos != std::string::npos) {
                 std::string variable_name = line.substr(0, equals_pos);
@@ -112,15 +114,20 @@ void readConfigFile() {
     configFile.close();
 }
 
-void barcodeReaderTask() {
+//Funcion que verifica la pistola //LED VERDE Y ROJO
+void barcodeReaderTask() { 
 	int fd = -1;
 	struct input_event event;
 	bool lshift = false;
 	bool rshift = false;
-
-	Led led0(14);
-	led0.blink();
-
+  //El led  ||blink = funciona|| ||.on .blink2 = Queda apagado|| ||.off = queda prendido||
+	Led led0(14); // Se asigna LED VERDE
+  Led led2(21); // Se asigna LED ROJO
+  led0.blink();
+  led2.on();
+	
+  std::cout << "LED VERDE encendido" << std::endl;
+  
 	string str = "";
 
 	while (true) {
@@ -129,13 +136,38 @@ void barcodeReaderTask() {
 			if (fd >= 0) {
 				ioctl(fd, EVIOCGRAB, (void *) 0);
 				close(fd);
-				fd = -1;
+				fd = -1; // -1 significa que no hay ninguna lectora. Segun puerto USB tiene o 5 o 6
 			}
-			fd = open(inputDevPath_Unitech.c_str(), O_RDONLY);
-			if (fd < 0)
+			
+      if (fd < 0){
+      fd = open(inputDevPath_Unitech.c_str(), O_RDONLY); // Se asigna lectora Unitech
+      std::cout << "Se asigna Lectora Unitech" << std::endl;
+      std::cout << fd << std::endl;
+        if(fd > 0){ //Si existe prende led
+        led0.blink(); //LED VERDE
+        led2.on(); //LED ROJO
+        }
+      
+      }
+      
+			if (fd < 0){ //Si fd es menor se asigna la datalogic
 				fd = open(inputDevPath_Datalogic.c_str(), O_RDONLY);
-			if (fd < 0)
-				this_thread::sleep_for(chrono::seconds(1));
+        std::cout << "No hay Lectora Unitech" << std::endl;
+        std::cout << "Se asigna Lectora Datalogic" << std::endl;
+        std::cout << fd << std::endl;
+        if(fd > 0){ //Si existe prende led
+          led0.blink(); //LED VERDE
+          led2.on(); //LED ROJO
+        }
+        
+        
+        }
+			if (fd < 0){ // luz roja
+        std::cout << "No hay ninguna lectota Datalogic o Unitech conectada" << std::endl;
+        led0.on();
+        led2.blink();
+				this_thread::sleep_for(chrono::seconds(3)); // Espera x segundos antes de arrancar el while
+        }
 			else
 				ioctl(fd, EVIOCGRAB, (void *) 1);
 		} else if (rd >= (ssize_t) sizeof(struct input_event)) {
@@ -162,6 +194,79 @@ void barcodeReaderTask() {
 	}
 }
 
+// funcion cuando la pistola hace lectura // LED AMARILLO
+void udpRecvTask() {
+	Led led1(15); //LED AMARILLO 
+	led1.on();   //Esto indica que inicialmente, el LED está apagado. Hay algo que invierte este metodo.
+	char readBuffer[1024]; //Se declara un búfer de lectura llamado readBuffer de 1024 bytes para almacenar los datos recibidos a través de UDP.
+ 
+	while (true) { //Se crea un Iterador
+		memset(readBuffer, 0, sizeof(readBuffer)); //En cada iteración del bucle, se borra el contenido del búfer de lectura readBuffer utilizando memset
+		ssize_t rlen = recv(sockfd, readBuffer, sizeof(readBuffer), 0); //Se utiliza recv para recibir datos a través de un socket UDP (sockfd) y se almacenan en readBuffer. La longitud        los datos recibidos se almacena en la variable rlen.
+		if (rlen > 0) { //Se verifica si se han recibido datos (rlen > 0). Si se han recibido datos, se imprime el contenido de readBuffer en la consola con la etiqueta "received".
+			cout << "received: " << readBuffer << endl;
+      //Si la longitud de los datos recibidos (rlen) es mayor que 4 (esto se refiere a la cantidad de bytes esperados en el paquete UDP), se procesan los datos. En este caso, se espera         que el paquete contenga información sobre el estado del LED y se extraen tres secciones de datos del paquete (idBuffer, ledBuffer y stBuffer) utilizando strncpy.
+			if (rlen > 4) {
+				int i = 0;
+				char idBuffer[4] = { };
+				char ledBuffer[2] = { };
+				char stBuffer[2] = { };
+				strncpy(idBuffer, readBuffer + i, sizeof(idBuffer) - 1);
+				i += sizeof(idBuffer) - 1;
+				strncpy(ledBuffer, readBuffer + i, sizeof(ledBuffer) - 1);
+				i += sizeof(ledBuffer) - 1;
+				strncpy(stBuffer, readBuffer + i, sizeof(stBuffer) - 1);
+				i += sizeof(stBuffer) - 1;
+//				uint8_t id = atoi(idBuffer);
+//				uint8_t led = atoi(ledBuffer);
+				uint8_t st = atoi(stBuffer); //Se convierten los datos de stBuffer a un entero (st) utilizando atoi.
+        
+        //Se utiliza una estructura switch para realizar diferentes acciones en función del valor de st. Use el std:out para saber que opcion elige la lectura.
+				switch (st) {
+				case 0:
+					led1.off();
+          // std::cout << "opcion 0" << std::endl;
+					break;
+				case 1:
+					led1.on(); // Estaba ON
+          // std::cout << "opcion 1" << std::endl;
+					break;
+				case 2:
+					led1.blink(); //Estaba blink
+          // std::cout << "opcion 2" << std::endl;      
+					break;
+				case 3:
+					led1.blink1();//Estaba blink1
+          // std::cout << "opcion 3" << std::endl;
+					break;
+				case 4:
+					led1.blink2();//Estaba blink2
+         // std::cout << "opcion 4" << std::endl;
+					break;
+				case 5:
+					led1.blink4();//Estaba blink4
+          // std::cout << "opcion 5" << std::endl;
+					break;
+				case 6:
+          // std::cout << "opcion 6" << std::endl; // Cuando lee usa este valor
+					led1.blink8();//Estaba blink8 
+          std::this_thread::sleep_for(std::chrono::seconds(1)); // Esto hace que despues del blink8 se corte 1 segundo el hilo del led para cambiar al proximo estado
+          led1.on(); 
+          
+					break;
+				default:
+					led1.off();//Estaba blink
+					break;
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
 void udpInit(char *serverIp, int serverPort) {
 	memset(&remote, 0, sizeof(struct sockaddr_in));
 	remote.sin_family = AF_INET;
@@ -184,7 +289,7 @@ void udpInit(char *serverIp, int serverPort) {
 
 void logMessage(const string& message) {
   ofstream logFile;
-  logFile.open("/home/pi/andon/log.txt", ios::app);
+  logFile.open("log.txt", ios::app);
 
   auto now = std::chrono::system_clock::now();
   auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -210,60 +315,6 @@ void udpSendTask() {
   }
 }
 
-
-void udpRecvTask() {
-	Led led1(15);
-	led1.off();
-	char readBuffer[1024];
-	while (true) {
-		memset(readBuffer, 0, sizeof(readBuffer));
-		ssize_t rlen = recv(sockfd, readBuffer, sizeof(readBuffer), 0);
-		if (rlen > 0) {
-			cout << "received: " << readBuffer << endl;
-			if (rlen > 4) {
-				int i = 0;
-				char idBuffer[4] = { };
-				char ledBuffer[2] = { };
-				char stBuffer[2] = { };
-				strncpy(idBuffer, readBuffer + i, sizeof(idBuffer) - 1);
-				i += sizeof(idBuffer) - 1;
-				strncpy(ledBuffer, readBuffer + i, sizeof(ledBuffer) - 1);
-				i += sizeof(ledBuffer) - 1;
-				strncpy(stBuffer, readBuffer + i, sizeof(stBuffer) - 1);
-				i += sizeof(stBuffer) - 1;
-//				uint8_t id = atoi(idBuffer);
-//				uint8_t led = atoi(ledBuffer);
-				uint8_t st = atoi(stBuffer);
-				switch (st) {
-				case 0:
-					led1.off();
-					break;
-				case 1:
-					led1.on();
-					break;
-				case 2:
-					led1.blink();
-					break;
-				case 3:
-					led1.blink1();
-					break;
-				case 4:
-					led1.blink2();
-					break;
-				case 5:
-					led1.blink4();
-					break;
-				case 6:
-					led1.blink8();
-					break;
-				default:
-					led1.off();
-					break;
-				}
-			}
-		}
-	}
-}
 
 char getChar(__u16 code, bool shift) {
 	uint8_t idx = code & 0xff;
